8824f7332e7749f9cc9e4b8aec018b50e050d87b
Ziad Emad <ziademad2023@gmail.com>
2025-05-10 03:01:35 +0300

    Update Courses Page

--- JS/main.js
+++ JS/main.js
@@ -1,56 +1,66 @@
- async function fetchCourses() {
-      try {
-        const res = await fetch('/Courses/courses.json'); // عدل المسار حسب مكان الملف
-        const courses = await res.json();
-        console.log(courses);
-        displayCourses(courses);
-        initSwiper(); // بعد ما الكروت تتولد
-      } catch (err) {
-        console.error("Faild: ", err);
-      }
-    }
-
-    function createCourseSlide(course) {
-      return `
+// All Courses
+const pathAllCourse = "/Courses/courses.json";
+const idForCourses = "courses-wrapper";
+// HTML Video
+const pathHtmlVideo = '/Courses/Html.json';
+const idForHtmlVideo = 'htmlCoursePage';
+// Bootstrap Video
+const pathBootstrapVideo = '/Courses/bootstrap.json';
+const idForBootstrapVideo = 'bootstrapCoursePage';
+//  Get Data
+async function fetchCourses(url,idForContainer) {
+  try {
+    const res = await fetch(url);
+    const courses = await res.json();
+    console.log(courses);
+    displayCourses(courses, idForContainer);
+  } catch (err) {
+    console.error("Faild: ", err);
+  }
+}
+// Create Card
+function createCourseSlide(course) {
+  return `
         <div class="swiper-slide d-flex justify-content-center">
           <div class="card course-card shadow-sm ">
             <img src="${course.thumbnail}" class="card-img-top " alt="${course.title}">
             <div class="card-body">
               <h6 class="card-title fw-bold">${course.title}</h6>
-              <h6 class="card-text text-muted">Instractor: <strong>${course.instractor}</strong></ا>
-              <a href="${course.pagePath}" class="btn btn-primary w-100 mt-3" onclick="event.preventDefault();">Start Course</a>
+              <h6 class="card-text text-muted">Instractor: <strong>${course.instractor}</strong></h6>
+              <a href="${course.pagePath}" target="blank" class="btn btn-primary w-100 mt-2" >${course.txtBtn}</a>
             </div>
           </div>
         </div>
       `;
-    }
-
-    function displayCourses(courses) {
-      const wrapper = document.getElementById('courses-wrapper');
-      wrapper.innerHTML = courses.map(createCourseSlide).join('');
-    }
-
-fetchCourses();
-
+}
+// Display Card
+function displayCourses(courses, idForContainer) {
+  const wrapper = document.getElementById(idForContainer);
+  wrapper.innerHTML = courses.map(createCourseSlide).join("");
+}
+// Set All Courses
+fetchCourses(pathAllCourse, idForCourses);
+// Set HTML Video
+fetchCourses(pathHtmlVideo,idForHtmlVideo)
+// Set Bootstrap Video
+fetchCourses(pathBootstrapVideo,idForBootstrapVideo)
 
 
 // Swiper
 var swiper = new Swiper(".mySwiper", {
-   slidesPerView: 3,
+  slidesPerView: 3,
   centeredSlides: true,
   spaceBetween: 30,
-  centerSlides: true,
-  loop: true,
-  fade: true,
+  //   loop: true,
   grabCursor: true,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
     dynamicBullets: true,
-
   },
   autoplay: {
-    delay: 3000,   
+    delay: 3000,
+    disableOnInteraction: false,
   },
   breakpoints: {
     0: {
